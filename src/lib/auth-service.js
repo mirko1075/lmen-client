c726ac308eec361d18c4a044c73f4f4044c08754
Mirko Siddi <mirko.siddi@gmail.com>
2020-12-08 21:49:13 +0100

    First commit

--- src/lib/auth-service.js
+++ src/lib/auth-service.js
@@ -4,44 +4,40 @@ class AuthService {
   constructor() {
     this.auth = axios.create({
       baseURL: "http://localhost:5000",
-      withCredentials: true
+      withCredentials: true,
     });
   }
 
-  signup( username, password ) {
+  signup(username, password) {
     const pr = this.auth
+
       .post("/auth/signup", { username, password })
       .then((response) => response.data);
-      // .then(({ data }) => data); // Shorter way of `.then((response) => response.data);`
+    // .then(({ data }) => data); // Shorter way of `.then((response) => response.data);`
 
     return pr;
   }
 
-  login( username, password ) {
+  login(username, password) {
     const pr = this.auth
       .post("/auth/login", { username, password })
       .then((response) => response.data);
-      
+
     return pr;
   }
 
   logout() {
-    const pr = this.auth
-      .get("/auth/logout")
-      .then((response) => response.data);
+    const pr = this.auth.get("/auth/logout").then((response) => response.data);
 
     return pr;
   }
 
   me() {
-    const pr = this.auth
-      .get("/auth/me")
-      .then((response) => response.data);
+    const pr = this.auth.get("/auth/me").then((response) => response.data);
 
     return pr;
   }
 }
-
 
 const authService = new AuthService();
 
